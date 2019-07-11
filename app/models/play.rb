590ef39f8edfd97f47bcb4a7780ef1abdc9ddf4d
yuto040308 <yuto.ge6@gmail.com>
2019-07-11 18:03:53 +0900

    event#index maked

--- app/models/play.rb
+++ app/models/play.rb
@@ -3,6 +3,9 @@ class Play < ApplicationRecord
     # refileでattachment_fieldを使う場合、ここに指定しないと動かない _idは外す
     attachment :play_image
 
+    has_many :events
     belongs_to :category, optional: true
+
+
 
 end
