c6eaa55b7712b1071eff55ae4835f448594afcf5
yuto040308 <yuto.ge6@gmail.com>
2019-08-18 15:42:58 +0900

    controller check play_controller 2/3 rspec

--- spec/controllers/play_controller_spec.rb
+++ spec/controllers/play_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper' # これは書く必要がある
+require "refile/file_double" # refileのテストをするため、この記述が必要。
 RSpec.describe PlaysController, type: :controller do
     describe '遊び一覧ページ #index' do
       context "遊び一覧ページが正しく表示される" do
@@ -99,7 +100,7 @@ RSpec.describe PlaysController, type: :controller do
                             category_id: 1,
                             play_title: "テストタイトル",
                             play_explain: "テスト説明です",
-                            play_image_id: "fjwoewjfiowjifel",  # 画像は文字になるので、適当な値を入力
+                            play_image: Refile::FileDouble.new("dummy", "logo.png", content_type: "image/png"),
                             play_delete_flg: 1
                         }
                     }
@@ -119,7 +120,7 @@ RSpec.describe PlaysController, type: :controller do
                             category_id: 1,
                             play_title: "テストタイトル",
                             play_explain: "テスト説明です",
-                            play_image_id: "fjwoewjfiowjifel",  # 画像は文字になるので、適当な値を入力
+                            play_image: Refile::FileDouble.new("dummy", "logo.png", content_type: "image/png"),
                             play_delete_flg: 1
                         }
                     }
@@ -148,6 +149,122 @@ RSpec.describe PlaysController, type: :controller do
                 expect(flash[:notice]).to eq("遊びを新規作成しました")
 
             end
+        end
+    end
+
+    describe '管理者遊び編集ページ #admin_edit' do
+        context "管理者遊び編集ページが正しく表示される" do
+
+            before do
+                # あらかじめFacrotyBotで登録しておいた管理者ユーザーを作成する
+                @user = FactoryBot.create(:admin_user)
+                # テスト用のデータを作成しておく
+                @play = Play.new(
+                    category_id: 1,
+                    play_title: "テストタイトル",
+                    play_explain: "テスト説明文"
+                )
+                @play.save
+                
+            end
+    
+            it 'リクエストは200 OKとなること' do
+                # deviseのヘルパー@userをログイン状態にする。
+                sign_in @user
+
+                # edit画面を表示
+                get :admin_edit, params: {id: @play.id}
+                expect(response.status).to eq 200
+            end
+        end
+    end
+
+    describe "管理者遊び更新ページ #admin_update" do
+        context "遊びの投稿が更新されるか" do
+
+            before do
+                # あらかじめFacrotyBotで登録しておいた管理者ユーザーを作成する
+                @user = FactoryBot.create(:admin_user)
+                # テスト用のデータを作成しておく
+                @play = Play.new(
+                    category_id: 1,
+                    play_title: "テストタイトル",
+                    play_explain: "テスト説明文",
+                    play_image: Refile::FileDouble.new("dummy", "logo.png", content_type: "image/png"),
+                    play_delete_flg: 1
+                )
+                @play.save
+                
+            end
+
+            it '記事が更新されるか' do
+                # deviseのヘルパー@userをログイン状態にする。
+                sign_in @user
+
+                # updateメゾットを呼ぶ
+                patch :admin_update, params: {
+                    id: @play.id, # 保存したidで検索をかける
+
+                    play: {
+                        category_id: 2,
+                        play_title: "テストタイトル２",
+                        play_explain: "テスト２説明です",
+                        # require "refile/file_double"を宣言したことにより、ダミーのファイルを指定できるようになった
+                        play_image: Refile::FileDouble.new("dummy", "logo2.png", content_type: "image/png"),
+                        play_delete_flg: 0
+                    }
+                }
+                # 変更されているか確認 reloadと記入する必要がある
+                expect(@play.reload.category_id).to eq 2
+                expect(@play.reload.play_title).to eq "テストタイトル２"
+                expect(@play.reload.play_explain).to eq "テスト２説明です"
+                expect(@play.reload.play_image_id).not_to be_nil
+                expect(@play.reload.play_delete_flg).to eq 0
+            end
+
+            it 'リダイレクトされること' do
+
+                # deviseのヘルパー@userをログイン状態にする。
+                sign_in @user
+
+                # DBに正常値を入れる
+                patch :admin_update, params: {
+                    id: @play.id, # 保存したidで検索をかける
+                        play: {
+                            category_id: 1,
+                            play_title: "テストタイトル",
+                            play_explain: "テスト説明です",
+                            play_image: Refile::FileDouble.new("dummy", "logo.png", content_type: "image/png"),
+                            play_delete_flg: 1
+                        }
+                    }
+                
+                # 正常型のリダイレクト先に移動していること
+                expect(response).to redirect_to admin_user_path
+            end
+
+            it 'flash[:notice]にメッセージが含まれているか' do
+
+                # deviseのヘルパー@userをログイン状態にする。
+                sign_in @user
+
+                # DBに正常値を入れる
+                post :admin_update, params: {
+                    id: @play.id, # 保存したidで検索をかける
+                    play: {
+                        category_id: 1,
+                        play_title: "テストタイトル",
+                        play_explain: "テスト説明です",
+                        play_image: Refile::FileDouble.new("dummy", "logo.png", content_type: "image/png"),
+                        play_delete_flg: 1
+                    }
+                }
+            
+                # flash[:notice]に想定通りのメッセージが含まれているか
+                expect(flash[:notice]).to eq("遊びを更新しました")
+
+            end
+
         end
     end
 
